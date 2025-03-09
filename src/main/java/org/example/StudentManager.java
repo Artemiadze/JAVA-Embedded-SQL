d56c5d2000dbb012ce43fa086380f4b2c0c22e2d
Artemiadze <t0mi.v@yandex.ru>
2025-03-09 13:07:21 +0300

    update function fir sqlj

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -1,242 +1,118 @@
 package org.example;
-import java.sql.*;
 
+import java.sql.*;
+import sqlj.runtime.ref.DefaultContext;
 
 public class StudentManager {
     private static final String URL = "jdbc:postgresql://localhost:5432/";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgre";
+    private DefaultContext ctx;
 
-    // Создание БД
-    public void createDatabase() {
-        System.out.println("Registering JDBC driver...");
-
-        System.out.println("Connecting to DB...");
-
-        System.out.println("Creating database...");
-
-        String SQL = "CREATE DATABASE student";
-        executeUpdate(SQL);
-        System.out.println("Database successfully created...");
-
-        createStudentTable(); // Создание самой таблицы
-        AddNecessaryFunction(); // Добавление на сервер хранимых функций
-    }
-
-    // Создание таблицы student после создания БД
-    private void createStudentTable() {
-        String newDbURL = "jdbc:postgresql://localhost:5432/student";
-
-        String createTableSQL = "CREATE TABLE students (" +
-                "id SERIAL PRIMARY KEY, " +
-                "name VARCHAR(100) NOT NULL, " +
-                "email VARCHAR(100) UNIQUE NOT NULL, " +
-                "group_name VARCHAR(50) NOT NULL" +
-                ");";
-
-
-        try (Connection conn = getConnection(newDbURL);
-             Statement stmt = conn.createStatement()) {
-
-            stmt.executeUpdate(createTableSQL);
-            System.out.println("Таблица students успешно создана в БД " + "student");
-
+    public StudentManager() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            ctx = new DefaultContext(conn);
         } catch (SQLException e) {
-            System.err.println("Ошибка при создании таблицы student: " + e.getMessage());
+            e.printStackTrace();
         }
-    }
-
-    // Удаление БД
-    public void dropDatabase() {
-        String SQL = "DROP DATABASE student";
-        executeUpdate(SQL);
-
-        System.out.println("Database successfully deleted...");
     }
 
     // Добавление процедур на сервер
-    public void AddNecessaryFunction(){
-        String newURL = "jdbc:postgresql://localhost:5432/student";
+    public void AddNecessaryFunction() throws SQLException {
+        #sql [ctx] {
+            CREATE OR REPLACE FUNCTION truncate_student() RETURNS VOID AS $$
+            BEGIN
+            TRUNCATE TABLE students RESTART IDENTITY CASCADE;
+            END;
+            $$ LANGUAGE plpgsql;
+        };
 
-        // Очистка таблицы Course
-        String TruncateSQL = "CREATE OR REPLACE FUNCTION truncate_student() RETURNS VOID AS $$\n" +
-                "BEGIN\n" +
-                "    TRUNCATE TABLE students RESTART IDENTITY CASCADE;\n" +
-                "END;\n" +
-                "$$ LANGUAGE plpgsql;";
+        #sql [ctx] {
+            CREATE OR REPLACE FUNCTION insert_student(insert_name VARCHAR, insert_email VARCHAR, insert_group VARCHAR)
+            RETURNS VOID AS $$
+            BEGIN
+            INSERT INTO students (name, email, group_name) VALUES (insert_name, insert_email, insert_group);
+            END;
+            $$ LANGUAGE plpgsql;
+        };
 
-        // Добавление студента
-        String InsertSQL = "CREATE OR REPLACE FUNCTION insert_student(insert_name VARCHAR, insert_email VARCHAR, insert_group VARCHAR)" +
-                "RETURNS VOID AS $$" +
-                "BEGIN" +
-                "    INSERT INTO students (name, email, group_name) VALUES (insert_name, insert_email, insert_group);" +
-                "END;" +
-                "$$ LANGUAGE plpgsql;";
+        #sql [ctx] {
+            CREATE OR REPLACE FUNCTION search_student_by_name(search_name VARCHAR)
+            RETURNS TABLE(id INT, name VARCHAR, email VARCHAR, group_name VARCHAR) AS $$
+            BEGIN
+            RETURN QUERY
+            SELECT students.id, students.name, students.email, students.group_name
+            FROM students
+            WHERE students.name ILIKE '%' || search_name || '%';
+            END;
+            $$ LANGUAGE plpgsql;
+        };
 
-        // Получение списка студентов по имени
-        String SearchNameSQL = "CREATE OR REPLACE FUNCTION search_student_by_name(search_name VARCHAR)\n" +
-                "RETURNS TABLE(id INT, name VARCHAR, email VARCHAR, group_name VARCHAR) AS $$\n" +
-                "BEGIN\n" +
-                "    RETURN QUERY \n" +
-                "    SELECT students.id, students.name, students.email, students.group_name\n" +
-                "    FROM students\n" +
-                "    WHERE students.name ILIKE '%' || search_name || '%';\n" +
-                "END;\n" +
-                "$$ LANGUAGE plpgsql;\n";
+        #sql [ctx] {
+            CREATE OR REPLACE FUNCTION update_student(update_id INT, update_name VARCHAR, update_email VARCHAR, update_group VARCHAR)
+            RETURNS VOID AS $$
+            BEGIN
+            UPDATE students SET name = update_name, email = update_email, group_name = update_group WHERE id = update_id;
+            END;
+            $$ LANGUAGE plpgsql;
+        };
 
-        // Обновление данных студента
-        String UpdateSQL = "CREATE OR REPLACE FUNCTION update_student(update_id INT, update_name VARCHAR, update_email VARCHAR, update_group VARCHAR) \n" +
-                "RETURNS VOID AS $$\n" +
-                "BEGIN\n" +
-                "    UPDATE students SET name = update_name, email = update_email, group_name = update_group WHERE id = update_id;\n" +
-                "END;\n" +
-                "$$ LANGUAGE plpgsql;";
+        #sql [ctx] {
+            CREATE OR REPLACE FUNCTION delete_student_by_email(delete_email VARCHAR)
+            RETURNS VOID AS $$
+            BEGIN
+            DELETE FROM students WHERE students.email = delete_email;
+            END;
+            $$ LANGUAGE plpgsql;
+        };
 
-        // Удаление студента по email
-        String DeleteSQL = "CREATE OR REPLACE FUNCTION delete_student_by_email(delete_email VARCHAR)" +
-                "RETURNS VOID AS $$" +
-                "BEGIN" +
-                "    DELETE FROM students WHERE students.email = delete_email;" +
-                "END;" +
-                "$$ LANGUAGE plpgsql;";
-
-
-        try (Connection conn = getConnection(newURL);
-             Statement stmt = conn.createStatement()) {
-
-            stmt.executeUpdate(TruncateSQL);
-            stmt.executeUpdate(InsertSQL);
-            stmt.executeUpdate(SearchNameSQL);
-            stmt.executeUpdate(UpdateSQL);
-            stmt.executeUpdate(DeleteSQL);
-
-            System.out.println("Процедуры успешно добавлены " + "student");
-
-        } catch (SQLException e) {
-            System.err.println("Ошибка при добавлений процедур в student: " + e.getMessage());
-        }
+        System.out.println("Процедуры успешно добавлены в student");
     }
 
-    // Добавление данных о студенте в таблицу
-    public void addStudent(String name, String email, String group) {
-        String newURL = "jdbc:postgresql://localhost:5432/student";
-
-        String callFunctionSQL = "SELECT insert_student(?, ?, ?);";
-        try (Connection conn = getConnection(newURL)) {
-            // Вызов функции для вставки данных
-            try (PreparedStatement pstmt = conn.prepareStatement(callFunctionSQL)) {
-                pstmt.setString(1, name);
-                pstmt.setString(2, email);
-                pstmt.setString(3, group);
-
-                pstmt.execute();
-                System.out.println("Student inserted successfully.");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    // Добавление студента
+    public void addStudent(String name, String email, String group) throws SQLException {
+        #sql [ctx] {
+            SELECT insert_student(:name, :email, :group);
+        };
+        System.out.println("Студент добавлен.");
     }
 
     // Поиск студента по имени
-    public String searchStudentByName(String name) {
-        String newURL = "jdbc:postgresql://localhost:5432/student";
-        String callFunctionSQL = "SELECT * FROM search_student_by_name(?);";
-
-        StringBuilder result = new StringBuilder();
-        try (Connection conn = getConnection(newURL)) {
-            // Вызов функции для обновления данных
-            try (PreparedStatement pstmt = conn.prepareStatement(callFunctionSQL)) {
-                pstmt.setString(1, name);
-                ResultSet rs = pstmt.executeQuery();
-
-                while (rs.next()) {
-                    result.append("ID: ").append(rs.getInt("id"))
-                            .append(", Имя: ").append(rs.getString("name"))
-                            .append(", Email: ").append(rs.getString("email"))
-                            .append(", Группа: ").append(rs.getString("group_name"))
-                            .append("\n");
-                }
-                System.out.println("Student was found successfully.");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public void searchStudentByName(String name) throws SQLException {
+        #sql iterator StudentIter (int id, String name, String email, String group_name);
+        StudentIter iter;
+        #sql [ctx] iter = {
+                SELECT * FROM search_student_by_name(:name);
+        };
+        while (iter.next()) {
+            System.out.println("ID: " + iter.id() + ", Name: " + iter.name() + ", Email: " + iter.email() + ", Group: " + iter.group_name());
         }
-        return result.toString();
+        iter.close();
     }
 
-    // Обновление данных о студенте
-    public void updateStudent(String name, String email, String group) {
-        String newURL = "jdbc:postgresql://localhost:5432/student";
-
-        String callFunctionSQL = "SELECT update_student(?, ?, ?);";
-        try (Connection conn = getConnection(newURL)) {
-            // Вызов функции для обновления данных
-            try (PreparedStatement pstmt = conn.prepareStatement(callFunctionSQL)) {
-                pstmt.setString(1, name);
-                pstmt.setString(2, email);
-                pstmt.setString(3, group);
-
-                pstmt.execute();
-                System.out.println("Student was updated successfully.");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    // Обновление студента
+    public void updateStudent(int id, String name, String email, String group) throws SQLException {
+        #sql [ctx] {
+            SELECT update_student(:id, :name, :email, :group);
+        };
+        System.out.println("Данные студента обновлены.");
     }
 
-    // Удаление данных о студенте по почте
-    public void deleteStudentByEmail(String name) {
-        String newURL = "jdbc:postgresql://localhost:5432/student";
-
-        String callFunctionSQL = "SELECT delete_student_by_email(?);";
-        try (Connection conn = getConnection(newURL)) {
-            // Вызов функции для вставки данных
-            try (PreparedStatement pstmt = conn.prepareStatement(callFunctionSQL)) {
-                pstmt.setString(1, name);
-
-                pstmt.execute();
-                System.out.println("Student was deleted successfully.");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    // Удаление студента по email
+    public void deleteStudentByEmail(String email) throws SQLException {
+        #sql [ctx] {
+            SELECT delete_student_by_email(:email);
+        };
+        System.out.println("Студент удалён.");
     }
 
     // Очистка таблицы
-    public void truncateStudentTable() {
-        String newURL = "jdbc:postgresql://localhost:5432/student";
-
-        String callFunctionSQL = "SELECT truncate_student();";
-        try (Connection conn = getConnection(newURL)) {
-            // Вызов функции для вставки данных
-            try (PreparedStatement pstmt = conn.prepareStatement(callFunctionSQL)) {
-                pstmt.execute();
-                System.out.println("Table was cleaned successfully.");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Connection getConnection(String url) throws SQLException {
-        return DriverManager.getConnection(url, USER, PASSWORD);
-    }
-
-    private void executeUpdate(String query, String... params) {
-        try (Connection conn = getConnection(URL);
-             CallableStatement stmt = conn.prepareCall(query)) {
-            for (int i = 0; i < params.length; i++) {
-                stmt.setString(i + 1, params[i]);
-            }
-            stmt.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public void truncateStudentTable() throws SQLException {
+        #sql [ctx] {
+            SELECT truncate_student();
+        };
+        System.out.println("Таблица очищена.");
     }
 }
+
